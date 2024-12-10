855e3e581a5c38eb2d738f505782faed04ad4d88
N-LAZAR0V <n.lazarov2020@pmg-pernik.com>
2024-12-10 11:38:56 +0200

    1.1

---
+++ DogProject/DogsApp.Infrastructure/Data/Infrastructure/ApplicationBuilderExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsApp.Infrastructure.Data.Infrastructure
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static async Task<IApplicationBuilder> PrepareDatabase(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+
+            var services = serviceScope.ServiceProvider;
+
+            var data = 
+
+        }
+    }
+}
