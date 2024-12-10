df9e7ba11870ac79e43a812015c48f2ac81c8e4a
N-LAZAR0V <n.lazarov2020@pmg-pernik.com>
2024-12-10 11:07:21 +0200

    v

--- 03.1.2.DOM манипулации 1/AvatarJ.js
+++
@@ -1,5 +0,0 @@
-let imgs = document.getElementsByTagName("img")
-for(let i=0; i<imgs.length;i++)
-{
-    imgs[i].width = 500;
-}
--- 03.1.2.DOM манипулации 1/JS-3J.js
+++
@@ -1,7 +0,0 @@
-let about = document.getElementsByTagName("h3")
-about[0].innerHTML = "Woooo, I'm all up in your webpage!"
-
-let about2 = document.getElementsByTagName("h1")
-about2[0].innerHTML = "Nikola Lazarov"
-
-
